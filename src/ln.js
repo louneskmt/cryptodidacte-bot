96eaefb08cb0237403babffca552478d31acda37
Lounès KSOURI <lounes.kmt@icloud.com>
2020-03-06 16:12:38 +0100

    Changing node address

--- src/ln.js
+++ src/ln.js
@@ -1,13 +1,8 @@
 var grpc = require('grpc');
-var fs = require("fs");
+var fs = require('fs');
 
-// Due to updated ECDSA generated tls.cert we need to let gprc know that
-// we need to use that cipher suite otherwise there will be a handhsake
-// error when we communicate with the lnd rpc server.
 process.env.GRPC_SSL_CIPHER_SUITES = 'HIGH+ECDSA'
 
-//  Lnd cert is at ~/.lnd/tls.cert on Linux and
-//  ~/Library/Application Support/Lnd/tls.cert on Mac
 var lndCert = fs.readFileSync('../certs/tls.cert');
 var credentials = grpc.credentials.createSsl(lndCert);
 
@@ -15,9 +10,9 @@ var m = fs.readFileSync('../certs/admin.macaroon');
 var macaroon = m.toString('hex');
 var meta = new grpc.Metadata().add('macaroon', macaroon);
 
-var lnrpcDescriptor = grpc.load("rpc.proto");
+var lnrpcDescriptor = grpc.load('rpc.proto');
 var lnrpc = lnrpcDescriptor.lnrpc;
-var client = new lnrpc.Lightning('some.address:10009', grpc.credentials.createInsecure());
+var client = new lnrpc.Lightning('84.100.44.54:10009', grpc.credentials.createInsecure());
 
 const pay_ln_invoice = (invoice) => {
   console.log("Invoice paid!")
