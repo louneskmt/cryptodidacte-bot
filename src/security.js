a77e41631212ddf1f5f26c7f4bd639619986b6ca
Lounès KSOURI <lounes.kmt@icloud.com>
2020-03-08 19:29:08 +0100

    Minor modifications

--- src/security.js
+++ src/security.js
@@ -7,8 +7,6 @@ const crypto = require('crypto')
  * @return string
  */
 module.exports.get_challenge_response = function(crc_token, consumer_secret) {
-
   hmac = crypto.createHmac('sha256', consumer_secret).update(crc_token).digest('base64')
-
   return hmac
 }
