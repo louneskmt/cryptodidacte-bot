8831bf76b9e50f8292aca2a15dad81d40178ca1a
Lounès KSOURI <lounes.kmt@icloud.com>
2020-03-08 19:28:22 +0100

    New Twitter functions / module

--- src/Twit.js
+++ src/Twit.js
@@ -1,5 +1,30 @@
 const Twit = require('twit');
 const twitterApp = require('../config.js');
-const T = new Twit(twitterApp);
+const Twitter = new Twit(twitterApp);
 
-module.exports = T;
+// @lounes_kmt -> 986994912565620736
+
+const sendTextMessage = (user_id, text) => {
+  var message = {
+    event: {
+      type: "message_create",
+      message_create: {
+        target: {
+          recipient_id: user_id
+        },
+        message_data: {
+          text: text
+        }
+      }
+    }
+  }
+
+  Twitter.post('direct_messages/events/new', message, function (err, data, response) {
+    console.log(data)
+    //console.log(response)
+  });
+}
+
+module.exports = {
+  sendTextMessage
+};
