faf6d1dd1bf0c990799f6cfd824c70633d4dc2dd
Lounès KSOURI <lounes.kmt@icloud.com>
2020-03-08 19:28:58 +0100

    Updating on DM event

--- src/events.js
+++ src/events.js
@@ -1,12 +1,20 @@
 var events = require('events');
+var lightning = require('./lightning.js');
+var Twitter = require('./Twit');
+
 var eventEmitter = new events.EventEmitter();
 
 var { pay_ln_invoice } = require('./ln.js');
 
 eventEmitter.on('dm', (message) => {
   if(message.message_data.text.startsWith('ln')) {
-    pay_ln_invoice(message.message_data.text);
+    lightning.sendPayment(message.message_data.text);
   }
-})
+
+  if(message.message_data.text.startsWith('Generate invoice for 200 sats')) {
+    invoice = lightning.addInvoice("Test", 200);
+    Twitter.sendTextMessage('986994912565620736', invoice);
+  }
+});
 
 module.exports.eventEmitter = eventEmitter;
