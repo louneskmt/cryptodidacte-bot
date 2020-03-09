1ce1a9ba9332883f7405358efd775d26791993e7
Lounès KSOURI <lounes.kmt@icloud.com>
2020-03-09 12:27:41 +0100

    Testing LN without Twitter

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.post('/webhook/twitter', function(request, response) {
   console.log(request.body);
   if(request.body.hasOwnProperty('direct_message_events')) {
-    events.eventEmitter.emit('dm', request.body.direct_message_events[0].message_create)
+    events.eventEmitter.emit('dm', request.body.direct_message_events[0].users[0].id, request.body.direct_message_events[0].message_create.message_data.text)
 
   }
   response.send('200 OK')
@@ -57,5 +57,5 @@ https.createServer({
 .listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'))
   Twitter.sendTextMessage('986994912565620736', 'Node app is running on port ' + app.get('port'));
-  events.eventEmitter.emit('dm', 'lnbc2u1p0xvf68pp5lfanv7339rphk8tlqjfawl2v075c9m4skdmd48hnm4uwhcucq2jqdqu2askcmr9wssx7e3q2dshgmmndp5scqzpgxqrrss0785tmdm7c0r7jeu7jk5nm589fnjql50tavglxkh6x0egyp6lslkn32spkgm5j46geh9y3c36gaqzsw3z85z79k6fzmp4lxj8aylxccpz84xm2');
+  events.eventEmitter.emit('dm', '986994912565620736', 'lnbc2u1p0xvf68pp5lfanv7339rphk8tlqjfawl2v075c9m4skdmd48hnm4uwhcucq2jqdqu2askcmr9wssx7e3q2dshgmmndp5scqzpgxqrrss0785tmdm7c0r7jeu7jk5nm589fnjql50tavglxkh6x0egyp6lslkn32spkgm5j46geh9y3c36gaqzsw3z85z79k6fzmp4lxj8aylxccpz84xm2');
 });
