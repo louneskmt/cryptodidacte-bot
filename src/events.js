0af23a9c70716b50cfe15da79fd130ca6530eb9d
Lounès KSOURI <lounes.kmt@icloud.com>
2020-03-09 12:27:41 +0100

    Testing LN without Twitter

--- src/events.js
+++ src/events.js
@@ -4,16 +4,16 @@ var lightning = require('./lightning.rest.js');
 
 var eventEmitter = new events.EventEmitter();
 
-eventEmitter.on('dm', (message) => {
+eventEmitter.on('dm', (user_id, message) => {
   if(message.message_data.text.startsWith('ln')) {
-    console.log("Paying invoice : ", message.message_data.text)
-    lightning.payInvoice(message.message_data.text);
+    console.log("Paying invoice : ", message)
+    lightning.payInvoice(message);
   }
 
   if(message.message_data.text.startsWith('Generate invoice for 200 sats')) {
     var invoice = lightning.generateInvoice(200, "Test");
     console.log("Generating invoice")
-    Twitter.sendTextMessage('986994912565620736', invoice);
+    Twitter.sendTextMessage(user_id, invoice);
   }
 });
 
