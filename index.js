fdfde3fe81b283c241ef26a211990c2f8c7d0544
Lounès KSOURI <lounes.kmt@icloud.com>
2020-03-09 21:16:05 +0100

    Get user ID #2

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.post('/webhook/twitter', function(request, response) {
   console.log(request.body)
   if(request.body.hasOwnProperty('direct_message_events')) {
     console.log(request.body.direct_message_events[0].message_create);
-    events.eventEmitter.emit('dm', Object.keys(request.body.direct_message_events[0].users), request.body.direct_message_events[0].message_create.message_data.text)
+    events.eventEmitter.emit('dm', Object.keys(request.body.users), request.body.direct_message_events[0].message_create.message_data.text)
   }
   response.send('200 OK')
 })
