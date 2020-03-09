93da1528c93c418d8cea8673add318f7b3ccb416
Lounès KSOURI <lounes.kmt@icloud.com>
2020-03-09 12:02:27 +0100

    Adding debug logs

--- src/events.js
+++ src/events.js
@@ -6,11 +6,13 @@ var eventEmitter = new events.EventEmitter();
 
 eventEmitter.on('dm', (message) => {
   if(message.message_data.text.startsWith('ln')) {
+    console.log("Paying invoice : ", message.message_data.text)
     lightning.payInvoice(message.message_data.text);
   }
 
   if(message.message_data.text.startsWith('Generate invoice for 200 sats')) {
     var invoice = lightning.generateInvoice(200, "Test");
+    console.log("Generating invoice")
     Twitter.sendTextMessage('986994912565620736', invoice);
   }
 });
