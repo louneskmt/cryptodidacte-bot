3393df5ca296c44042a7ab0932914f5fc8599070
Lounès KSOURI <lounes.kmt@icloud.com>
2020-03-11 23:42:19 +0100

    PNG to Jpeg

--- src/qrcode.js
+++ src/qrcode.js
@@ -2,7 +2,7 @@ var QRCode = require('qrcode');
 var path = require('path')
 
 const generateQRCode = (text, callback) => {
-  var filePath = path.join(__dirname, '../assets/temp/qrcode-' + text.substring(0,30).toLowerCase().replace(/ /g, '_') + '.png');
+  var filePath = path.join(__dirname, '../assets/temp/qrcode-' + text.substring(0,30).toLowerCase().replace(/ /g, '_') + '.jpeg');
   QRCode.toFile(filePath, text, {
     color: {
       dark: '#FC0030',  // Blue dots
