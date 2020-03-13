84371e811020e6019b780e3720fafc931ede1fb7
Lounès KSOURI <lounes.kmt@icloud.com>
2020-03-13 01:10:09 +0100

    Supporting redirect from http to https

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var Twitter = require('./src/Twit');
 
-Twitter.Twitter.post('account_activity/all/dev/webhooks.json?url=https%3A%2F%2Flouneskmt.com%2Fwebhook%2Ftwitter', function(err, body, res) {
+Twitter.Twitter.post('account_activity/all/dev/webhooks.json?url=https%3a%2f%2flouneskmt.com%2fwebhook%2ftwitter', function(err, body, res) {
   console.log(body)
 })
