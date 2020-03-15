aa098ba2be69047f6759c639651314b34b59414a
Lounès KSOURI <lounes.kmt@icloud.com>
2020-03-15 01:49:53 +0100

    Undo getCollection

--- src/database.js
+++ src/database.js
@@ -36,7 +36,7 @@ const insertOneDocument = (collection, newEntry, callback) => {
   connect((db) => {
     // Get the collection
     // Insert some documents
-    db.getCollection(collection).insertOne(newEntry, function(err, result) {
+    db.collection(collection).insertOne(newEntry, function(err, result) {
       assert.equal(err, null);
       console.log("Inserted 1 document into the collection");
       callback(result);
@@ -62,7 +62,7 @@ const removeDocument = (collection, query, callback) => {
   connect((db) => {
     // Get the documents collection
     // Delete document
-    db.getCollection(collection).deleteOne({ a : 3 }, function(err, result) {
+    db.collection(collection).deleteOne(query, function(err, result) {
       assert.equal(err, null);
       assert.equal(1, result.result.n);
       console.log("Document removed");
@@ -76,7 +76,7 @@ const updateDocument = (collection, query, modification, callback) => {
   connect((db) => {
     // Get the documents collection
     // Update document where a is 2, set b equal to 1
-    db.getCollection(collection).updateOne(query
+    db.collection(collection).updateOne(query
       , { $set: modification }, function(err, result) {
       assert.equal(err, null);
       assert.equal(1, result.result.n);
