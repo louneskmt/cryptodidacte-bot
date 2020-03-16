65d4dc346c221e2275ded6da839569500789818f
Lounès KSOURI <lounes.kmt@icloud.com>
2020-03-16 16:01:17 +0100

    Testing withtout query but as path

--- src/lightning.rest.js
+++ src/lightning.rest.js
@@ -80,10 +80,10 @@ const generateInvoice = (value, memo, successCallback, errorCallback) => {
 }
 
 const getInvoiceData = (payment_request) => {
-  var r_hash_str = Buffer.from(payment_request).toString('base64');
-
+  var r_hash = Buffer.from(payment_request).toString('base64');
+  console.log(r_hash);
   var options = {
-    url: `https://kfmprmnblmf262qcj7vf7lh7lqzrybruj5vvzx7rkpt3kafejwtvydad.onion:10080/v1/invoice?r_hash=${r_hash_str}`,
+    url: `https://kfmprmnblmf262qcj7vf7lh7lqzrybruj5vvzx7rkpt3kafejwtvydad.onion:10080/v1/invoice/${r_hash}`,
     strictSSL: false,
   	agentClass: Agent,
     agentOptions: {
