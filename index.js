312a081b8fe9a2f8dcceec87ea92f1fc1dea636b
Killian <clota974@gmail.com>
2020-03-18 00:38:17 +0400

    Added JSON log

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {__} = require("./src/logger.js");
+const {__,  __json} = require("./src/logger.js");
 
 // Require Events handler and fs to read files
 const eventEmitter = require('./src/events.js');
