9e524b9a9cc0bc8069bb6c7ed12f513c9c9a6747
Killian <clota974@gmail.com>
2020-04-05 17:12:06 +0400

    Testing table

--- public/index.js
+++ public/index.js
@@ -1,5 +1,8 @@
 $(function(){
     $("#auth-connect").click(() => connect() );
+
+    $("#data-table tr td:first-child").click(selectElementRow)
+    $("#data-table tr th:first-child").click(selectAllTabEl)
 });
 
 let connect = async function(){
@@ -16,9 +19,31 @@ let sleep = async secs => {
     })
 }
 
-let showIndex = async function(){
+let showIndex = async () => {
     $("#sect-auth").addClass("hideEffect");
     await sleep(.3);
     $("#sect-auth").addClass("dis-none");
     $("#sect-index").removeClass("dis-none");
 }
+
+let selectElementRow = function (ev){
+    $(this).parents("tr").toggleClass("selected");    
+    checkIfAllSelected();
+}
+
+let selectAllTabEl = function (ev){
+    $(this).toggleClass("selected");
+    if( $(this).hasClass("selected") ){
+        $(this).parents("table").children("tbody").children("tr").each(async function(ix, el){
+            await sleep(ix*0.05);
+            $(el).addClass("selected")
+        });    
+    }else{
+        $(this).parents("table").children("tbody").children("tr").each(async function(ix, el){
+            await sleep(ix*0.05);
+            $(el).removeClass("selected")
+        });       
+    }
+}
+
+let
