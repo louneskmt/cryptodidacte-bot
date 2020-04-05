7550c99624ae4afd8d760c30cfb25147ecf98719
Killian <clota974@gmail.com>
2020-04-05 23:55:22 +0400

    Update createAdminUser.js

--- dev/createAdminUser.js
+++ dev/createAdminUser.js
@@ -1,5 +1,6 @@
 const readline = require("readline");
 const Db = require("../src/database.js");
+const crypto = require("crypto")
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -12,12 +13,33 @@ let prompt = async text => {
     })
 }
 
+let hashPasswordd = ({username, password}) =>{
+    let salt = process.env.SALT+password+"*#*"+process.env.SALT
+    let shasum = crypto.createHash("sha1").update(salf).digest();
+    return shasum;
+} 
+
 (async function(){
     console.log("************************* CONNECT TO DATABASE *************************")
     let username = await prompt("Enter your username: ");
-    let password = await prompt("Enter your password: ", {method: "mask"});
+    let password = await prompt("Enter your password: ");
 
     console.log("\n TRYING TO CONNECT \n");
     
-    let db = new Db(`mongodb://${username}:${password}@localhost:27017/adminAuthTable`);
+    let db = new Db("users",`mongodb://${username}:${password}@localhost:27017/adminWebsite`);
+    await db.connect();
+    if(!db.connected){
+        console.error("Couldn't log in");
+        return process.exit(1);
+        // END
+    }
+
+    console.log("You are connected to the authAdminTable");
+    console.log("************************* CREATING USER *************************");
+    let nUsername = await prompt("Enter username of new user:")
+    let nPassword = await prompt("Enter password of new user:");
+
+
+    db.insert({username: nUsername, password: nPassword})
+
 })();
