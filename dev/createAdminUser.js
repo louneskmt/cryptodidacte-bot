87092321e8380042636fec3bfc7908ab21c3b024
Killian <clota974@gmail.com>
2020-04-07 01:22:24 +0400

    Update createAdminUser.js

--- dev/createAdminUser.js
+++ dev/createAdminUser.js
@@ -15,6 +15,8 @@ let prompt = async text => {
 
 let hashPassword = ({username, password}) =>{
     password = crypto.createHash("sha256").update("1d34caabaa37"+password+"ead78d1d5753583562b6").digest("hex");
+    console.log(password);
+    
     let salt = process.env.SALT+"*#*"+username+"--"+password+"*#*"+process.env.SALT
     let shasum = crypto.createHash("sha256").update(salt).digest("hex");
     return shasum;
