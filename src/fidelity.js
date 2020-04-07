806fcb4aeafe7ae5147f2627b888218e47287250
Lounès KSOURI <lounes.kmt@icloud.com>
2020-04-07 22:56:38 +0400

    Added fidelity module + processEvent

---
+++ src/fidelity.js
@@ -0,0 +1,49 @@
+const {__} = require("../logger.js");
+const Database = require("../src/database.js");
+
+const { websiteDbConfig } = require('../config.js'); 
+
+const processEvent = async (event, data) => {
+    let db = new Database("fidelity", `mongodb://${websiteDbConfig.user}:${websiteDbConfig.password}@localhost:27017/fidelity`);
+
+    await db.insert("history", data);
+
+    let reward;
+    switch (event) {
+        case 'favorite':
+            reward = 2;
+            break;
+        case 'retweet':
+            reward = 5;
+            break;
+        case 'quote':
+            reward = 6;
+            break;
+        case 'reply':
+            reward = 3;
+            break;
+        default:
+            reward = 0;
+            break;
+    }
+
+    db.find("users", {user_id: data.user_id}).then((docs) => {
+        if(docs.length === 0) {
+            db.insert("users", {user_id: data.user_id}, {
+                user_id: data.user_id,
+                user_name: data.user_name,
+                balance: reward
+            });
+        } else {
+            db.update("users", {
+                user_id: data.user_id,
+                user_name: data.user_name,
+                balance: reward
+            });
+        }
+    });
+};
+
+module.exports = {
+    processEvent
+}
