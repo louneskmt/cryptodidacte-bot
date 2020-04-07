80de5d8111159910addb0f3f4dc3bc016dae14a6
Lounès KSOURI <lounes.kmt@icloud.com>
2020-04-07 15:03:50 +0400

    Added Cryptodidacte ID to config.js

--- config.js
+++ config.js
@@ -14,7 +14,8 @@ const twitterConfig = {
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   webhook_id: '1238264407626592257',
   user_id_bot: '1235621426125774850',
-  admin: ["986994912565620736", "955781788743454721", "1175249514745212928"] // @lounes_kmt, @Cryptodidacte @cillianklota
+  user_id_cryptodidacte: '955781788743454721',
+  admin: ['986994912565620736', '955781788743454721', '1175249514745212928'] // @lounes_kmt, @Cryptodidacte @cillianklota
 };        
 
 const databaseConfig = {
