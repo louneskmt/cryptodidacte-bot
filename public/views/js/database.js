31441fd3b6eaf14af8615b151ede0219212a9f7a
Killian <clota974@gmail.com>
2020-04-09 00:19:16 +0400

    Update database.js

--- public/views/js/database.js
+++ public/views/js/database.js
@@ -1,5 +1,38 @@
 viewDetails = {}
 onViewLoaded = async function (obj) {
+    let selectElementRow = function (ev) {
+        $(this).parents("tr").toggleClass("selected");
+        checkIfAllSelected();
+    }
+
+    let selectAllTabEl = function (ev) {
+        $(this).toggleClass("selected");
+
+        if ($(this).hasClass("selected")) {
+            $("#data-table").children("tbody").children("tr").each(async function (ix, el) {
+                await sleep(ix * 0.05);
+                $(el).addClass("selected")
+            });
+        } else {
+            $("#data-table").children("tbody").children("tr").each(async function (ix, el) {
+                await sleep(ix * 0.05);
+                $(el).removeClass("selected")
+            });
+        }
+    }
+
+    let updateTableRows = () => {
+        $(".data-table-check").click(selectElementRow)
+        $("#data-table tr:first-child td").each((ix, el) => {
+            if (ix == 0) return true;
+            let width = $(el).width();
+
+            $(`.data-thead tr th:nth-child(${ix+1})`).css({
+                width
+            })
+        })
+    }
+
     let {
         collection,
         filter = {},
@@ -64,39 +97,4 @@ onViewLoaded = async function (obj) {
     });
 
     $("*[click-role=showIndex]").click(showIndex);
-
-
-    let selectElementRow = function (ev) {
-        $(this).parents("tr").toggleClass("selected");
-        checkIfAllSelected();
-    }
-
-    let selectAllTabEl = function (ev) {
-        $(this).toggleClass("selected");
-
-        if ($(this).hasClass("selected")) {
-            $("#data-table").children("tbody").children("tr").each(async function (ix, el) {
-                await sleep(ix * 0.05);
-                $(el).addClass("selected")
-            });
-        } else {
-            $("#data-table").children("tbody").children("tr").each(async function (ix, el) {
-                await sleep(ix * 0.05);
-                $(el).removeClass("selected")
-            });
-        }
-    }
-
-    let updateTableRows = () => {
-        $(".data-table-check").click(selectElementRow)
-        $("#data-table tr:first-child td").each((ix, el) => {
-            if (ix == 0) return true;
-            let width = $(el).width();
-
-            $(`.data-thead tr th:nth-child(${ix+1})`).css({
-                width
-            })
-        })
-
-    }
 }
