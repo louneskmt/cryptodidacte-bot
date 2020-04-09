299e18eed982fb8b915b1ff135e0408298b4b47c
Lounès KSOURI <lounes.kmt@icloud.com>
2020-04-09 13:13:59 +0400

    Updated fidelity to check if event already exists in history

--- src/fidelity.js
+++ src/fidelity.js
@@ -4,13 +4,28 @@ const ethereum = require('./ethereum.js');
 
 const { websiteDbConfig, ethereumConfig } = require('../config.js'); 
 
-const processEvent = async (event, data) => {
+/*
+eventData Object sample:
+{
+    user_id: user_id,
+    user_name: user_name,
+    tweet_id: tweet_id,
+    type: 'retweet',
+    target_tweet_id,
+    timestamp
+}
+*/
+const processEvent = async (eventData) => {
+    // Connect to DB
     let db = new Database("fidelity", `mongodb://${websiteDbConfig.user}:${websiteDbConfig.password}@localhost:27017/fidelity`);
     await db.connect();
-    await db.insert("history", data);
+
+    let research = await db.find("history", {user_id: eventData.user_id, type: eventData.type, target_tweet_id: eventData.target_tweet_id});
+    if(research.length != 0) return;
+    await db.insert("history", eventData);
 
     let reward;
-    switch (event) {
+    switch (eventData.event_type) {
         case 'favorite':
             reward = 2;
             break;
@@ -28,29 +43,31 @@ const processEvent = async (event, data) => {
             break;
     }
 
-    db.find("users", {user_id: data.user_id}).then(async (docs) => {
+    db.find("users", {user_id: eventData.user_id}).then(async (docs) => {
         if(typeof docs == undefined || docs.length === 0) {
             await db.insert("users", {
-                user_id: data.user_id,
-                user_name: data.user_name,
+                user_id: eventData.user_id,
+                user_name: eventData.user_name,
                 balance: reward
             });
         } else {
             await db.update("users", { 
-                filter: {user_id: data.user_id},
+                filter: {user_id: eventData.user_id},
                 edit:{balance: reward},
                 mode: 'inc'
             });
         }
         await db.disconnect();
     });
+
+    check(eventData);
 };
 
 const payout = (user_id) => {
     let db = new Database("fidelity", `mongodb://${websiteDbConfig.user}:${websiteDbConfig.password}@localhost:27017/fidelity`);
     const CDT = new ethereum.ERC20(contractAddress, CryptodidacteTokenABI);
 
-    db.find("users", {user_id: data.user_id}).then((docs) => {
+    db.find("users", {user_id: eventData.user_id}).then((docs) => {
         if(!docs[0].address) {
             __(`There is not any address linked to this account (@${docs[0].user_name})`);
             return;
