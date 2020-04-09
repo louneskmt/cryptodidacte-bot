f91b36f9c7c4d23747cb83cb45f8ebf0b2ec1c2d
Killian <clota974@gmail.com>
2020-04-09 09:08:03 +0400

    Added viewParams

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ app.get("/index", function(req, res){
       res.redirect(`/connect`);
     });
   }else{
-    ejs.renderFile(__dirname + "/public/index.ejs", {view: ""}, function(err,str){
+    ejs.renderFile(__dirname + "/public/index.ejs", {view: "", viewParams: ""}, function(err,str){
       if(err) __(err,9);
       res.status(200).send(str);
     })
