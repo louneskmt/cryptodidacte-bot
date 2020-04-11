3b0378ced9927df8b1f3604fdcbe94fe0906380c
Killian <clota974@gmail.com>
2020-04-12 00:33:55 +0400

    Solution not found

--- src/helpers/insertVariablesInTemplate.js
+++ src/helpers/insertVariablesInTemplate.js
@@ -1,8 +1,11 @@
+const { __ } = require('../logger.js');
+
 /* eslint-disable no-param-reassign */
 const insertVariablesInTemplate = (messageData, params) => {
   let { text } = messageData;
 
   for (const key in params) {
+    __(text, 2);
     const regex = new RegExp(`%{(${key.toString()})}`, 'gm');
     text = text.replace(regex, params[key]);
   }
