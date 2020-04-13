a90d3123c10384415119d2949bdb8a89f3b0f267
Killian <clota974@gmail.com>
2020-04-13 19:29:22 +0400

    Corrected GUI bugs (#10)

--- src/database/schemas/plugins/loadUIDescription.js
+++ src/database/schemas/plugins/loadUIDescription.js
@@ -1,9 +1,14 @@
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable global-require */
 /* eslint-disable no-unused-vars */
+const { __ } = require('../../../logger.js');
+
 function loadUIDescription() {
   const UIDescription = require(`../descriptions/${this.modelName}.json`);
-  if (!UIDescription) throw new Error(`Model ${this.modelName} doesn't have description.`);
+  if (!UIDescription) {
+    __(`Tried to load model ${this.modelName} but it was not fount`, 0);
+    throw new Error(`Model ${this.modelName} doesn't have description.`);
+  }
   return UIDescription;
 }
 
