18acd943ddb1db22152617f7e740456d40578fd6
Lounès Ksouri <dev@louneskmt.com>
2020-04-16 19:07:26 +0400

    Updated functions

--- src/actions/fidelity/linkAddress.js
+++ src/actions/fidelity/linkAddress.js
@@ -1,14 +1,16 @@
 const Twitter = require('../../Twitter.js');
-const { waitForMessage } = require('../../helpers/pending.js');
+const { waitForPattern } = require('../../helpers/pending.js');
 const { end } = require('../global.js');
 
 const messageTemplates = require('../../../data/message_templates.json');
+const validators = require('../../helpers/validators.js');
 
 async function linkAddress(params) {
   const { userId } = params;
   Twitter.sendMessage(userId, messageTemplates.fidelity.link);
-  const response = await waitForMessage(userId);
+  const response = await waitForPattern(userId, { validator: validators.isEthereumAddress });
   if (!response) return end(params, 'Timeout, please try again');
+
   Twitter.sendTextMessage(userId, response.message);
   end(params);
 }
--- src/actions/fidelity/withdraw.js
+++ src/actions/fidelity/withdraw.js
@@ -1,12 +1,12 @@
 const Twitter = require('../../Twitter.js');
-const { waitForMessage } = require('../../helpers/pending.js');
+const { waitForPattern } = require('../../helpers/pending.js');
 const { end } = require('../global.js');
 const messageTemplates = require('../../../data/message_templates.json');
 
 async function withdrawCDT(params) {
   const { userId } = params;
   Twitter.sendMessage(userId, messageTemplates.fidelity.selectWithDrawAddress);
-  const response = await waitForMessage(userId);
+  const response = await waitForPattern(userId);
   end(params);
 }
 
