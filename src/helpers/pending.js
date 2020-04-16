7487cac73f2f3e39eb80a8653b6ac437fa2c76f8
Lounès Ksouri <dev@louneskmt.com>
2020-04-16 19:07:20 +0400

    Added validator to pending

--- src/helpers/pending.js
+++ src/helpers/pending.js
@@ -1,5 +1,8 @@
 const botEvents = require('../events/botEvents.js');
 const { UserStatus } = require('../database/mongoose.js');
+const { retry } = require('../actions/global.js');
+
+const messageTemplates = require('../../data/message_templates.json');
 
 const resolvePending = (params) => {
   const { userId } = params;
@@ -7,19 +10,26 @@ const resolvePending = (params) => {
   botEvents.emit(eventName, params);
 };
 
-const waitForMessage = async (userId) => {
+const waitForPattern = async (userId, { validator = () => true } = {}) => {
   UserStatus.set(userId, 'pending');
   return new Promise((resolve, reject) => {
     const eventName = `pending-${userId}`;
-    botEvents.once(eventName, (newParams) => {
-      resolve(newParams);
-      UserStatus.del(userId);
-    });
+
     setTimeout(() => resolve(), 600000);
+    while (true) {
+      botEvents.once(eventName, (newParams) => {
+        if (validator(newParams.message)) {
+          UserStatus.del(userId);
+          return resolve(newParams);
+        }
+        const replyMessage = messageTemplates.validators_errors[validator.name] || messageTemplates.global.reply;
+        retry(newParams, replyMessage);
+      });
+    }
   });
 };
 
 module.exports = {
   resolvePending,
-  waitForMessage,
+  waitForPattern,
 };
