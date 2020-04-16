a526c23f64def239528ec8b233f1d700e820a29b
Lounès Ksouri <dev@louneskmt.com>
2020-04-16 20:44:37 +0400

    Solved problem

--- src/helpers/pending.js
+++ src/helpers/pending.js
@@ -14,15 +14,20 @@ const waitForPattern = async (userId, { validator = () => true } = {}) => {
   UserStatus.set(userId, 'pending');
   return new Promise((resolve, reject) => {
     const eventName = `pending-${userId}`;
+    botEvents.removeAllListeners(eventName);
 
-    setTimeout(() => resolve(), 600000);
-    botEvents.once(eventName, (newParams) => {
+    let timeout = setTimeout(() => resolve(), 600000);
+    botEvents.on(eventName, (newParams) => {
       if (validator(newParams.message)) {
         UserStatus.del(userId);
+        botEvents.removeAllListeners(eventName);
         return resolve(newParams);
       }
       const replyMessage = messageTemplates.validators_errors[validator.name] || messageTemplates.global.reply;
       retry(newParams, replyMessage);
+
+      clearTimeout(timeout);
+      timeout = setTimeout(() => resolve(), 600000);
     });
   });
 };
