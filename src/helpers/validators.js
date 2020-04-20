02bf52a97d439839ae6ea9d23f302a87c059fa65
Lounès Ksouri <dev@louneskmt.com>
2020-04-20 13:59:26 +0400

    Added validator for invoice

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-param-reassign */
 const { isAddress } = require('web3-utils');
+const lightning = require('../../lightning.rest.js');
 
 /**
  * Checks if the given string is an address
  *
- * @method isAddress
+ * @method isEthereumAddress
  * @param {String} address the given HEX address
  * @return {Boolean}
  */
@@ -12,6 +13,23 @@ const isEthereumAddress = function (address) {
   return isAddress(address);
 };
 
+/**
+ * Checks if the given string is an Lightning Network payment request (invoice)
+ *
+ * @method isLightningInvoice
+ * @param {String} invoice the given string invoice
+ * @return {Promise} resolves {Boolean}
+ */
+const isLightningInvoice = async function (invoice) {
+  return new Promise((resolve, reject) => {
+    lightning.getInvoiceData(invoice)
+      .then(() => resolve(true))
+      .catch(() => resolve(false));
+  });
+};
+
+
 module.exports = {
   isEthereumAddress,
+  isLightningInvoice,
 };
