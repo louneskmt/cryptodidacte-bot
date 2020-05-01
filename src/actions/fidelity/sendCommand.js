ae70b10049f368a146e10ff75775d1c2c052276e
Lounès Ksouri <dev@louneskmt.com>
2020-05-01 18:13:41 +0400

    Added received payment notification
    Fixed issue
    
    
    Fixed 'undefined' isntead of current balance
    
    
    Updated received message

--- src/actions/fidelity/sendCommand.js
+++ src/actions/fidelity/sendCommand.js
@@ -5,23 +5,26 @@ const { end } = require('../global.js');
 const messageTemplates = require('../../../data/message_templates.json');
 const insertVariablesInTemplate = require('../../helpers/insertVariablesInTemplate.js');
 
-function sendCommand(params, args) {
+async function sendCommand(params, args) {
   const { userId, messageData } = params;
 
-  const mentions = messageData.user_mentions;
+  const mentions = messageData.entities.user_mentions;
   if (mentions.length > 1) return end(params, { description: 'Please provide only one recipient.' });
   if (mentions.length === 0) return end(params, { description: 'Please provide one recipient.' });
 
   const amount = args[0];
-  const from = { id_str: userId };
-  const to = mentions[0];
-
   if (!amount) {
     return end(params, { description: insertVariablesInTemplate(messageTemplates.fidelity.error, { err: 'Please enter valid amount and retry.' }) });
   }
 
+  const from = await Twitter.getUserInfo({ userId });
+  const to = mentions[0];
+
   sendTokens(from, to, amount)
-    .then((result) => end(params, { description: insertVariablesInTemplate(messageTemplates.fidelity.sendOk, { to: to.screen_name, amount, balance: result.balance }) }))
+    .then((balance) => {
+      Twitter.sendMessage(to.id_str, insertVariablesInTemplate(messageTemplates.fidelity.received, { sender: from.screen_name, amount }));
+      end(params, { description: insertVariablesInTemplate(messageTemplates.fidelity.sendOk, { to: to.screen_name, amount, balance }) });
+    })
     .catch((err) => end(params, { description: insertVariablesInTemplate(messageTemplates.fidelity.error, { err: err.message }) }));
 }
 
