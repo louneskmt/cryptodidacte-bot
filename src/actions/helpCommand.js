6502242d8c7769832a21f185eae42fabd1db2f07
Lounès Ksouri <dev@louneskmt.com>
2020-06-09 17:55:49 +0400

    Updated message

--- src/actions/helpCommand.js
+++ src/actions/helpCommand.js
@@ -2,10 +2,10 @@ const Twitter = require('../Twitter.js');
 
 const messageTemplates = require('../../data/message_templates.json');
 
-async function helpCommand(params) {
+async function helpCommand(params, args) {
   const { userId } = params;
 
-  await Twitter.sendMessage(userId, messageTemplates.global.intro);
+  if (args[0] === 'full') await Twitter.sendMessage(userId, messageTemplates.global.intro);
   await Twitter.sendMessage(userId, messageTemplates.global.helpCommands);
 }
 
